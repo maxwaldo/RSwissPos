74a6115c5a6c10c7d87ac24af4b7c4f28a146e9f
Maxime Walder <49658646+maxwaldo@users.noreply.github.com>
2022-10-11 11:56:46 +0200

    Update get_party_pos.R

--- R/get_party_pos.R
+++ R/get_party_pos.R
@@ -1,11 +1,11 @@
 
 get_party_pos <- function(party=NA, year=NA) {
   
-  if(!is.na(party) & !party %in% c("CVP", "FDP", "SVP", "SP", "GPS")) stop("Wrong party name. Please use: CVP, FDP, SVP, SP or GPS as party name")
+  if(!is.na(party) & any(party %in% c("CVP", "FDP", "SVP", "SP", "GPS"))) stop("Wrong party name. Please use: CVP, FDP, SVP, SP or GPS as party name")
   
   if(!is.na(year) & class(year)!="numeric") stop("year argument needs to be numeric.")
   
-  if(!is.na(year) & (year>2020|year<1970)) stop("year needs to be between 1970 and 2020")
+  if(!is.na(year) & (any(year>2022)|any(year<1970))) stop("year needs to be between 1970 and 2020")
   
   data(dataParty)
   
