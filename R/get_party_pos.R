a68ac2d195c14995cae3217400abe431051e8a4e
Maxime Walder <49658646+maxwaldo@users.noreply.github.com>
2022-10-11 11:11:25 +0200

    Change file path to fit the directories

--- R/get_party_pos.R
+++ R/get_party_pos.R
@@ -1,12 +1,13 @@
+
 get_party_pos <- function(party=NA, year=NA) {
   
   if(!is.na(party) & !party %in% c("CVP", "FDP", "SVP", "SP", "GPS")) stop("Wrong party name. Please use: CVP, FDP, SVP, SP or GPS as party name")
   
   if(!is.na(year) & class(year)!="numeric") stop("year argument needs to be numeric.")
   
-  if(!is.na(year) & (year>2022|year<1965)) stop("year needs to be between 1970 and 2020")
+  if(!is.na(year) & (year>2020|year<1970)) stop("year needs to be between 1970 and 2020")
   
-  load("data/Parties' position over time.Rda")
+  load("./data/Parties' position over time.Rda")
   
   if (is.na(year) & is.na(party)) {
     
@@ -16,19 +17,21 @@ get_party_pos <- function(party=NA, year=NA) {
   
   if(!is.na(year) & is.na(party)) {
     
-    data_party <- data_party[data_party$year %in% year,]
+    dataParty <- dataParty[dataParty$year %in% year,]
     
   }
   
   if(is.na(year) & !is.na(party)) {
     
-    data_party <- data_party[data_party$party %in% party,]
+    dataParty <- dataParty[dataParty$party %in% party,]
     
   }
   
   if(!is.na(year) & !is.na(party)) {
     
-    data_party <- data_party[data_party$party %in% party & data_party$year %in% year,]
+    dataParty <- dataParty[dataParty$party %in% party & dataParty$year %in% year,]
     
   }
+  
+  
 }
