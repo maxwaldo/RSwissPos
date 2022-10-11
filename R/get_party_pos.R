f53bacd40a69525e8874e1c24321a6268a9fa7a5
Maxime Walder <49658646+maxwaldo@users.noreply.github.com>
2022-10-11 12:08:48 +0200

    Change if conditions

--- R/get_party_pos.R
+++ R/get_party_pos.R
@@ -1,11 +1,11 @@
 
 get_party_pos <- function(party=NA, year=NA) {
   
-  if(!is.na(party) & any(party %in% c("CVP", "FDP", "SVP", "SP", "GPS"))) stop("Wrong party name. Please use: CVP, FDP, SVP, SP or GPS as party name")
+  if(!is.na(party) & any(party %in% c("CVP", "FDP", "SVP", "SP", "GPS"))==FALSE) stop("Wrong party name. Please use: CVP, FDP, SVP, SP or GPS as party name")
   
   if(!is.na(year) & class(year)!="numeric") stop("year argument needs to be numeric.")
   
-  if(!is.na(year) & (any(year>2022)|any(year<1965))) stop("year needs to be between 1965 and 2022")
+  if(!is.na(year) & (any(year>2022)==FALSE|any(year<1965)==FALSE)) stop("year needs to be between 1965 and 2022")
   
   data(dataParty)
   
