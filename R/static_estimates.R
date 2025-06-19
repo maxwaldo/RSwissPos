11b84141d31e8c4067bf5b8294f64fdcd3780274
Maxime Walder <49658646+maxwaldo@users.noreply.github.com>
2025-06-19 15:16:39 +0200

    Add files via upload

---
+++ R/static_estimates.R
@@ -0,0 +1,27 @@
+#' Posterior estimate for ballot proposal of a static Bayesian IRT model
+#'
+#' Data with posterior estimates of a static Bayesian IRT based on
+#' the computation in Walder (2025). This data enables estimating the
+#' position of cantons and municipalities on the party competition space.
+#'
+#' @docType data
+#'
+#' @usage data(static_estimates)
+#'
+#' @format A data frame with 453 rows and 6 variables:
+#' \describe{
+#'   \item{anr}{identification number of the ballot}
+#'   \item{Discrimination.2.5}{2.5th percentile of the discrimination parameter posterior estimate}
+#'   \item{Discrimination.25}{25th percentile of the discrimination parameter posterior estimate}
+#'   \item{Discrimination.50}{50th percentile of the discrimination parameter posterior estimate}
+#'   \item{Discrimination.75}{75th percentile of the discrimination parameter posterior estimate}
+#'   \item{Discrimination.97.5}{97.5th percentile of the discrimination parameter posterior estimate}
+#'
+#' @keywords datasets
+#'
+#' @references Walder (2025) Latent Ideological Positions of Swiss Parties and Subsets of the Population
+#'
+#'
+#' @examples
+#' data(static_estimates)
+"static_estimates"
